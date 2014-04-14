210b463d1c969d63557fda9cd46689f5ed5efd68
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-04-14 18:43:24 -0400

    work on test refactor

---
+++ index.js
@@ -0,0 +1,24 @@
+var ss = require('simple-statistics')
+    explode = require('./explode'),
+    point = require('./point'),
+t.explode = explode
+t.point = point
+
+module.exports = function(features, done){
+  t.explode(features, function(err, vertices){
+    var averageX, 
+        averageY, 
+        xs = [], 
+        ys = []
+    
+    _.each(vertices.features, function(v){
+      xs.push(v.geometry.coordinates[0])
+      ys.push(v.geometry.coordinates[1])
+    })
+
+    averageX = ss.mean(xs)
+    averageY = ss.mean(ys)
+
+    done(err, t.point(averageX, averageY))
+  })
+}
