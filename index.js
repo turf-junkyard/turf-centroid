098d300c9cfa59aa7233a493fdad91ba66776f17
Lyzi Diamond <lyzidiamond@gmail.com>
2015-01-21 14:15:13 -0800

    Conform to turf doc style guide

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ var each = require('turf-meta').coordEach;
 var point = require('turf-point');
 
 /**
- * Calculates the centroid of a polygon Feature or
- * FeatureCollection using the geometric mean of all vertices.
+ * Takes a {@link Feature} or {@link FeatureCollection} of any type and calculates the centroid using the geometric mean of all vertices.
  * This lessens the effect of small islands and artifacts when calculating
  * the centroid of a set of polygons.
  *
  * @module turf/centroid
  * @param {FeatureCollection} fc a {@link Feature} or FeatureCollection of any type
- * @return {Point} a Point showing the centroid of the input feature(s)
+ * @return {Point} a Point feature at the centroid of the input feature(s)
  * @example
  * var poly = turf.polygon([[
  * 	[105.818939,21.004714],
